c444c4899af68c6d5f1f2be1f3a2c332f7b224e7
hvrdummy123 <119740893+hvrdummy123@users.noreply.github.com>
2025-03-03 21:27:57 +0530

    Delete Phase2/restaurantService directory

--- Phase2/restaurantService/src/main/java/com/example/restaurantService/repository/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restaurantService.repository;
-
-import com.example.restaurantService.entity.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-}
